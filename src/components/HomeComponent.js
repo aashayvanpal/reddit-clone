0413956e74121b642af6aaa65e1b17a56e0650ed
Aashay <arkhamknight.aashay@gmail.com>
2022-07-28 18:57:35 +0530

    v2 with posts

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,18 @@
 import { Link } from 'react-router-dom'
 import { Buffer } from 'buffer'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const HomeComponent = () => {
+
+    const [subscribers, setSubscribers] = useState([])
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        getToken()
+        console.log('useEffect check')
+    }, [])
+
     const getToken = async () => {
         var code = ''
         var urlParams;
@@ -21,10 +32,10 @@ const HomeComponent = () => {
 
         try {
 
-            
+
             var clientID = ""
             var clientSecret = ""
-            var returnCode = "statecheck"
+            var returnCode = ""
 
             const credentials = Buffer.from(`${clientID}:${clientSecret}`).toString("base64")
             console.log(credentials)
@@ -39,17 +50,77 @@ const HomeComponent = () => {
             })
 
             let body = await response.json()
-            console.log(body)
+            // console.log(body)
+            console.log('token', body.access_token)
+
+            localStorage.setItem('token', body.access_token)
+
         }
         catch (e) { console.log('wrong', e) }
     }
 
+    const getSubscribers = async () => {
+        try {
+
+            axios.get('https://oauth.reddit.com/subreddits/mine/subscriber', {
+                headers: {
+                    Authorization: `bearer ${localStorage.getItem('token')}`
+                }
+            })
+                .then((response) => response.data.data.children)
+                .then(subscribers => {
+                    const subscribersArray = subscribers.map(subscriber => {
+                        console.log(subscriber)
+                        return subscriber.data.title
+                    })
+
+                    setSubscribers(subscribersArray)
+
+                })
+
+        }
+        catch (e) { console.log('wrong', e) }
+
+    }
+
+    const getPosts = async () => {
+        try {
+            axios.get('https://oauth.reddit.com/best', {
+                headers: {
+                    Authorization: `bearer ${localStorage.getItem('token')}`
+                }
+            })
+                .then((response) => {
+                    console.log('best response:', response)
+                    // console.log('data:', response.data.data.children)
+                    const posts = response.data.data.children.map(post => {
+                        return post.data.title
+                    })
+                    console.log(posts)
+                    setPosts(posts)
+                })
+
+        }
+        catch (e) { console.log('wrong', e) }
+
+    }
     return (
         <div>
             <h1>Hi this is home component</h1>
-            <button onClick={getToken}>get access token</button>
+            {/* <button onClick={getToken}>get access token</button> */}
+            <button onClick={getSubscribers}>get Subscribers</button>
+            <button onClick={getPosts}>get Posts</button>
             <Link to='/'>back</Link>
-        </div>
+            <br />
+            <br />
+
+            Subsriber - {subscribers.length}
+            {subscribers.map(subscriber => <div key={subscriber}>{subscriber}<br /></div>)
+            }
+
+            Posts - {posts.length}
+            {posts.map(post => <div key={post}>{post}<br /></div>)}
+        </div >
     )
 }
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,10 +6,8 @@ var clientSecret = "o7vuAK_Ph6pOB06N2rM3gJRt3Qplpw"
 var returnCode = "statecheck"
 
 const LoginForm = () => {
-    const redirectUser = async (e) => {
-        e.preventDefault()
-        const account = window.open(`https://www.reddit.com/api/v1/authorize?client_id=${clientID}&response_type=code&state=statecheck&redirect_uri=http://localhost:3000/home&duration=permanent&scope=identity,save,edit,submit`, "_self")
-
+    const redirectUser = async () => {
+        const account = window.open(`https://www.reddit.com/api/v1/authorize?client_id=${clientID}&response_type=code&state=statecheck&redirect_uri=http://localhost:3000/home&duration=permanent&scope=identity,save,edit,submit,mysubreddits,read`, "_self")
     }
     return (
         <div>
